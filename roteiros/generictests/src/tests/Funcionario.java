94a4786602f3907d6b0ac11fbd851b376d7633d8
Gerson <gerson.junior@ccc.ufcg.edu.br>
2016-06-04 19:38:39 -0300

    adicao de roteiros

---
+++ roteiros/generictests/src/tests/Funcionario.java
@@ -0,0 +1,5 @@
+package tests;
+
+public class Funcionario extends Pessoa {
+
+}
---
+++ roteiros/generictests/src/tests/OrdenedList.java
@@ -0,0 +1,12 @@
+package tests;
+
+import java.util.Collection;
+import java.util.List;
+
+public class OrdenedList<T> {
+
+    public void ca(Pessoa e) {
+
+    }
+
+}
