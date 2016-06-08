7812f20aaf0bab56be2e4d51530565c25c7c47db
Gerson <gerson.junior@ccc.ufcg.edu.br>
2016-06-08 12:51:17 -0300

    renomeacao de pastas

--- roteiros/roteiro(estudo)/algoritmos/src/algoritmos/OrdenadorTest.java
+++ roteiros/roteiro(estudo)/algoritmos/src/algoritmos/OrdenadorTest.java
@@ -12,16 +12,35 @@ public class OrdenadorTest {
         this.ordenador = new Ordenador<>();
     }
 
-    @Test
     public void bubbleSortTest() {
         Integer[] lista = { 5, 1, 2, 3, 4 };
-        ordenador.mergeSort(lista);
+        ordenador.bubbleSort(lista);
         ordenador.imprimeLista(lista);
 
         Integer[] lista2 = { 6, 4, 2, 8, 1, 9, 7, 3, 5 };
-        ordenador.mergeSort(lista2);
+        ordenador.bubbleSort(lista2);
         ordenador.imprimeLista(lista2);
+    }
+    
+    public void selectionSortTest() {
+        Integer[] lista = { 5, 1, 2, 3, 4 };
+        ordenador.selectionSort(lista);
+        ordenador.imprimeLista(lista);
 
+        Integer[] lista2 = { 6, 4, 2, 8, 1, 9, 7, 3, 5 };
+        ordenador.selectionSort(lista2);
+        ordenador.imprimeLista(lista2);
+    }
+    
+    @Test
+    public void insertionSortTest() {
+        Integer[] lista = { 5, 1, 2, 3, 4 };
+        ordenador.insertionSort(lista);
+        ordenador.imprimeLista(lista);
+
+        Integer[] lista2 = { 6, 4, 2, 8, 1, 9, 7, 3, 5 };
+        ordenador.insertionSort(lista2);
+        ordenador.imprimeLista(lista2);
     }
 
 }
