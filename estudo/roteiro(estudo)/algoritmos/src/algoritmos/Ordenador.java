81b23801c9762424633919125e950097251571aa
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2016-07-11 14:00:35 -0300

    merge sort

--- estudo/roteiro(estudo)/algoritmos/src/algoritmos/Ordenador.java
+++ estudo/roteiro(estudo)/algoritmos/src/algoritmos/Ordenador.java
@@ -1,16 +1,22 @@
 package algoritmos;
 
 public class Ordenador<E extends Comparable<E>> {
-    public void bubbleSort(E[] lista) {
+
+    public void ordena(E[] lista) {
+        // insertionSort(lista);
+        // bubbleSort(lista);
+        // selectionSort(lista);
+        mergeSort(lista);
+    }
+
+    private void bubbleSort(E[] lista) {
         boolean houveTroca;
         do {
             houveTroca = false;
             int ultimoOrdenado = lista.length - 1;
             for (int i = 0; i < ultimoOrdenado; i++) {
                 if (lista[i].compareTo(lista[i + 1]) > 0) {
-                    E aux = lista[i];
-                    lista[i] = lista[i + 1];
-                    lista[i + 1] = aux;
+                    troca(lista, i, i + 1);
                     houveTroca = true;
                     ultimoOrdenado--;
                 }
@@ -18,7 +24,7 @@ public class Ordenador<E extends Comparable<E>> {
         } while (houveTroca);
     }
 
-    public void selectionSort(E[] lista) {
+    private void selectionSort(E[] lista) {
         for (int i = 0; i < lista.length - 1; i++) {
             int menor = i;
             for (int j = i; j < lista.length; j++) {
@@ -26,23 +32,80 @@ public class Ordenador<E extends Comparable<E>> {
                     menor = j;
                 }
             }
-            E aux = lista[i];
-            lista[i] = lista[menor];
-            lista[menor] = aux;
+            troca(lista, i, menor);
         }
     }
 
-    public void insertionSort(E[] lista) {
-        for (int i = 0; i < lista.length - 1; i++) {
-            int elemento = i + 1;
-            while(elemento > 0 && lista[elemento].compareTo(lista[i]) < 0) {
-                elemento --;
+    private void insertionSort(E[] lista) {
+        for (int i = 0; i < lista.length; i++) {
+            int elemento = i;
+            while (elemento > 0
+                    && lista[elemento].compareTo(lista[elemento - 1]) < 0) {
+                troca(lista, elemento, elemento - 1);
+                elemento--;
             }
-            E aux = lista[i];
-            lista[i] = lista[elemento];
-            lista[elemento] = aux;
-            
+
         }
+    }
+
+    private void troca(E[] lista, int i, int j) {
+        E aux = lista[i];
+        lista[i] = lista[j];
+        lista[j] = aux;
+    }
+
+    private E[] listaAuxiliar;
+
+    @SuppressWarnings("unchecked")
+    private void mergeSort(E[] lista) {
+        this.listaAuxiliar = (E[]) new Comparable[lista.length];
+        mergeSort(lista, 0, lista.length - 1);
+    }
+
+    private void mergeSort(E[] lista, int inicio, int fim) {
+        if (inicio < fim) {
+        
+        int meio = inicio + (fim - inicio) / 2;
+        mergeSort(lista, inicio, meio);
+        mergeSort(lista, meio + 1, fim);
+
+        ordena(lista, inicio, meio, fim);
+        }
+
+    }
+
+    private void ordena(E[] lista, int inicio, int fim, int meio) {
+
+        for (int i = inicio; i <= fim; i++) {
+            listaAuxiliar[i] = lista[i];
+        }
+
+        int i = inicio;
+        int j = meio + 1;
+        int k = inicio;
+
+        while (i <= meio && j <= fim) {
+            if (listaAuxiliar[i].compareTo(lista[j]) <= 0) {
+                lista[k] = listaAuxiliar[i];
+                i++;
+            } else {
+                lista[k] = listaAuxiliar[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i <= meio) {
+            lista[k] = listaAuxiliar[i];
+            i++;
+            k++;
+
+        }
+    }
+
+    public E[] merge(E[] lista1, E[] lista2) {
+        return null;
     }
 
     public void imprimeLista(E[] lista) {
@@ -52,4 +115,5 @@ public class Ordenador<E extends Comparable<E>> {
         }
         System.out.println("]");
     }
+
 }
--- estudo/roteiro(estudo)/algoritmos/src/algoritmos/OrdenadorTest.java
+++ estudo/roteiro(estudo)/algoritmos/src/algoritmos/OrdenadorTest.java
@@ -1,5 +1,6 @@
 package algoritmos;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,40 +8,61 @@ public class OrdenadorTest {
 
     private Ordenador<Integer> ordenador;
 
+    private Integer[] l1D = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    private Integer[] l1O = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    private Integer[] l2D = { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+    private Integer[] l2O = { 1, 1, 1, 2, 2, 2, 3, 3, 3 };
+
+    private Integer[] l3D = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    private Integer[] l3O = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    private Integer[] l4D = {};
+    private Integer[] l4O = {};
+
+    private Integer[] l5D = { 0 };
+    private Integer[] l5O = { 0 };
+
     @Before
     public void setUp() throws Exception {
         this.ordenador = new Ordenador<>();
     }
 
-    public void bubbleSortTest() {
-        Integer[] lista = { 5, 1, 2, 3, 4 };
-        ordenador.bubbleSort(lista);
-        ordenador.imprimeLista(lista);
-
-        Integer[] lista2 = { 6, 4, 2, 8, 1, 9, 7, 3, 5 };
-        ordenador.bubbleSort(lista2);
-        ordenador.imprimeLista(lista2);
+    @Test
+    public void testel1() {
+        ordenador.ordena(l1D);
+        Assert.assertArrayEquals(l1D, l1O);
     }
-    
-    public void selectionSortTest() {
-        Integer[] lista = { 5, 1, 2, 3, 4 };
-        ordenador.selectionSort(lista);
-        ordenador.imprimeLista(lista);
 
-        Integer[] lista2 = { 6, 4, 2, 8, 1, 9, 7, 3, 5 };
-        ordenador.selectionSort(lista2);
-        ordenador.imprimeLista(lista2);
+    @Test
+    public void testel2() {
+        ordenador.ordena(l2D);
+        Assert.assertArrayEquals(l2D, l2O);
+    }
+
+    @Test
+    public void testel3() {
+        ordenador.ordena(l3D);
+        Assert.assertArrayEquals(l3D, l3O);
+    }
+
+    @Test
+    public void testel4() {
+        ordenador.ordena(l4D);
+        Assert.assertArrayEquals(l4D, l4O);
+    }
+
+    @Test
+    public void testel5() {
+        ordenador.ordena(l5D);
+        Assert.assertArrayEquals(l5D, l5O);
     }
     
     @Test
-    public void insertionSortTest() {
-        Integer[] lista = { 5, 1, 2, 3, 4 };
-        ordenador.insertionSort(lista);
-        ordenador.imprimeLista(lista);
+    public void testemerge() {
+        Integer[] resultado = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9};
+        Assert.assertArrayEquals(ordenador.merge(l1O, l2O), resultado);
 
-        Integer[] lista2 = { 6, 4, 2, 8, 1, 9, 7, 3, 5 };
-        ordenador.insertionSort(lista2);
-        ordenador.imprimeLista(lista2);
     }
 
 }
