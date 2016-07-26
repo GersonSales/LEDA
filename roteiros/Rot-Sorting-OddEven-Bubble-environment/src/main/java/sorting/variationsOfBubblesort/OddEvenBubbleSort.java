3b639fdacb07e84a96f4af6b4cf5968457cd0e46
Gerson <gerson.junior@ccc.ufcg.edu.br>
2016-07-26 10:39:08 -0300

    edicao oddEven

--- roteiros/Rot-Sorting-OddEven-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/OddEvenBubbleSort.java
+++ roteiros/Rot-Sorting-OddEven-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/OddEvenBubbleSort.java
@@ -13,82 +13,74 @@ import sorting.AbstractSorting;
  * considering neighbours (odd). After that, the algorithm performs a merge
  * between elements indexed by even and odd numbers.
  */
-public class OddEvenBubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
-	@Override
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		if (!validaArray(array, leftIndex, rightIndex))
-			return;
+public class OddEvenBubbleSort<T extends Comparable<T>>
+        extends AbstractSorting<T> {
+    @Override
+    public void sort(T[] array, int leftIndex, int rightIndex) {
+        if (!validaArray(array, leftIndex, rightIndex))
+            return;
 
-		boolean swapped;
+        boolean swapped;
 
-		do {
-			swapped = false;
+        do {
+            swapped = false;
+            for (int i = leftIndex; i < rightIndex -1 ; i++) {
+                if (array[i].compareTo(array[i + 2]) > 0) {
+                    util.Util.swap(array, i, i + 2);
+                    swapped = true;
+                }
+            }
+        } while (swapped);
 
-			for (int i = leftIndex; i < rightIndex - 1; i += 2) {
-				if (array[i].compareTo(array[i + 2]) > 0) {
-					util.Util.swap(array, i, i + 2);
-					swapped = true;
-				}
-			}
+        T[] result = Arrays.copyOf(array, array.length);
+        
+        int i = leftIndex;
+        int j = leftIndex + 1;
+        int k = leftIndex;
 
-			for (int i = leftIndex + 1; i < rightIndex - 1; i += 2) {
-				if (array[i].compareTo(array[i + 2]) > 0) {
-					util.Util.swap(array, i, i + 2);
-					swapped = true;
-				}
-			}
+        while (i <= rightIndex && j <= rightIndex) {
+            if (result[i].compareTo(result[j]) <= 0) {
+                array[k] = result[i];
+                i += 2;
+            } else {
+                array[k] = result[j];
+                j += 2;
+            }
+            k++;
+        }
 
-		} while (swapped);
+        while (i <= rightIndex) {
+            array[k] = result[i];
+            i += 2;
+            k++;
+        }
 
-		T[] result = Arrays.copyOf(array, array.length);
+        while (j <= rightIndex) {
+            array[k] = result[j];
+            j += 2;
+            k++;
+        }
 
-		int i = leftIndex;
-		int j = leftIndex + 1;
-		int k = leftIndex;
+    }
 
-		while (i <= rightIndex && j <= rightIndex) {
-			if (result[i].compareTo(result[j]) <= 0) {
-				array[k] = result[i];
-				i += 2;
-			} else {
-				array[k] = result[j];
-				j += 2;
-			}
-			k++;
-		}
+    private boolean validaArray(T[] array, int leftIndex, int rightIndex) {
+        if (array == null)
+            return false;
+        if (rightIndex < leftIndex)
+            return false;
+        if (leftIndex < 0)
+            return false;
+        if (rightIndex > array.length)
+            return false;
+        if (array.length < 2)
+            return false;
 
-		while (i <= rightIndex) {
-			array[k] = result[i];
-			i += 2;
-			k++;
-		}
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null)
+                return false;
+        }
 
-		while (j <= rightIndex) {
-			array[k] = result[j];
-			j += 2;
-			k++;
-		}
-
-	}
-
-	private boolean validaArray(T[] array, int leftIndex, int rightIndex) {
-		if (array == null)
-			return false;
-		if (rightIndex < leftIndex)
-			return false;
-		if (leftIndex < 0)
-			return false;
-		if (rightIndex > array.length)
-			return false;
-		if (array.length < 2)
-			return false;
-
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == null)
-				return false;
-		}
-
-		return true;
-	}
+        return true;
+    }
 
 }
