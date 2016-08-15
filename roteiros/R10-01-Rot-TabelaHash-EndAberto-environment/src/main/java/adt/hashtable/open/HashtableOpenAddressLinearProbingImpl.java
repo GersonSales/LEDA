7132c391f58e027d672c57b5dcada9dddf1b2703
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2016-08-15 15:45:36 -0300

    inicializacao do R10-01

--- roteiros/R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ roteiros/R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -3,11 +3,9 @@ package adt.hashtable.open;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
-		AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size,
-			HashFunctionClosedAddressMethod method) {
+	public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
 		super(size);
 		hashFunction = new HashFunctionLinearProbing<T>(size, method);
 		this.initiateInternalTable(size);
@@ -15,20 +13,75 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isFull()) {
+			throw new HashtableOverflowException();
+		}
+
+		if (element != null) {
+			for (int i = 0; i <= elements; i++) {
+				int elementHash = getHash(element, i);
+				if (isAvaible(elementHash)) {
+					if (!this.table[elementHash].equals(element)) {
+						table[elementHash] = element;
+						elements++;
+					}
+					return;
+				} else {
+					COLLISIONS++;
+				}
+			}
+		}
+	}
+
+	private boolean isAvaible(int hash) {
+		if (this.table[hash] instanceof HashtableElement) {
+			return false;
+		}
+		return true;
+	}
+
+	private int getHash(T element, int probe) {
+		return ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element, probe);
 	}
 
 	@Override
 	public void remove(T element) {
+		if (element != null) {
+
+			for (int i = 0; i <= elements; i++) {
+				int elementHash = getHash(element, i);
+				if (this.table[elementHash] != null) {
+					if (this.table[elementHash].equals(element)) {
+						this.table[elementHash] = new DELETED();
+						elements--;
+					}
+				}
+			}
+
+		}
+
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+
+			for (int i = 0; i <= elements; i++) {
+				int elementHash = getHash(element, i);
+				if (this.table[elementHash] != null) {
+					if (this.table[elementHash].equals(element)) {
+						return element;
+					}
+				} else {
+					return null;
+				}
+
+			}
+		}
+
+		return null;
 	}
 
 	@Override
