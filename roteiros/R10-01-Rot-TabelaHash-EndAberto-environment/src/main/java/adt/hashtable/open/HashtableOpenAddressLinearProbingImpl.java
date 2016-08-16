a07d8b863bca89cf7a65f9cc82dcb0eaea260779
Gerson <gerson.junior@ccc.ufcg.edu.br>
2016-08-15 21:58:18 -0300

    finalizacao do R10-01

--- roteiros/R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ roteiros/R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -3,107 +3,84 @@ package adt.hashtable.open;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable>
+        extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
-		super(size);
-		hashFunction = new HashFunctionLinearProbing<T>(size, method);
-		this.initiateInternalTable(size);
-	}
+    public HashtableOpenAddressLinearProbingImpl(int size,
+            HashFunctionClosedAddressMethod method) {
+        super(size);
+        hashFunction = new HashFunctionLinearProbing<T>(size, method);
+        this.initiateInternalTable(size);
+    }
 
-	@Override
-	public void insert(T element) {
-		if (isFull()) {
-			throw new HashtableOverflowException();
-		}
+    @Override
+    public void insert(T element) {
 
-		if (element != null) {
+        if (isFull()) {
+            throw new HashtableOverflowException();
+        }
 
-			for (int i = 0; i < this.table.length; i++) {
-				int elementHash = getHash(element, i);
-				if (isAvaible(elementHash)) {
-					if (this.table[elementHash] == null) {
-						table[elementHash] = element;
-						elements++;
-					} else {
-						if (!this.table[elementHash].equals(element)) {
-							table[elementHash] = element;
-							elements++;
+        if (indexOf(element) < 0) {
+            for (int i = 0; i < this.table.length; i++) {
+                int elementHash = getHash(element, i);
 
-						}
-					}
-					return;
-				} else {
-					COLLISIONS++;
-				}
-			}
-		}
-	}
+                if (isAvaible(elementHash)) {
+                    this.table[elementHash] = element;
+                    this.elements++;
+                    return;
+                } else {
+                    this.COLLISIONS++;
+                }
+            }
+        }
+    }
 
-	private boolean isAvaible(int hash) {
-		if (this.table[hash] instanceof HashtableElement) {
-			return false;
-		}
-		return true;
-	}
+    private boolean isAvaible(int elementHash) {
+        if (this.table[elementHash] instanceof HashtableElement) {
+            return false;
+        }
+        return true;
+    }
 
-	private int getHash(T element, int probe) {
-		return ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element, probe);
-	}
+    private int getHash(T element, int probe) {
+        return ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element,
+                probe);
+    }
 
-	@Override
-	public void remove(T element) {
-		if (element != null) {
+    @Override
+    public void remove(T element) {
+        int elementIndex = indexOf(element);
+        if (elementIndex >= 0) {
+            this.table[elementIndex] = new DELETED();
+            this.elements--;
+        }
 
-			for (int i = 0; i <= this.table.length; i++) {
-				int elementHash = getHash(element, i);
-				if (this.table[elementHash] != null) {
-					if (this.table[elementHash].equals(element)) {
-						this.table[elementHash] = new DELETED();
-						elements--;
-					}
-				}
-			}
+    }
 
-		}
+    @SuppressWarnings("unchecked")
+    @Override
+    public T search(T element) {
+        int elementIndex = indexOf(element);
+        if (elementIndex > 0) {
+            return (T) this.table[elementIndex];
+        }
+        return null;
+    }
 
-	}
+    @Override
+    public int indexOf(T element) {
+        if (element != null) {
+            for (int i = 0; i <= this.table.length; i++) {
+                int elementHash = getHash(element, i);
+                if (this.table[elementHash] != null) {
+                    if (this.table[elementHash].equals(element)) {
+                        return elementHash;
+                    }
+                }
+            }
+        }
+        return -1;
 
-	@Override
-	public T search(T element) {
-		if (element != null) {
-
-			for (int i = 0; i <= this.table.length; i++) {
-				int elementHash = getHash(element, i);
-				if (this.table[elementHash] != null) {
-					if (this.table[elementHash].equals(element)) {
-						return element;
-					}
-				} else {
-					return null;
-				}
-
-			}
-		}
-
-		return null;
-	}
-
-	@Override
-	public int indexOf(T element) {
-		if (element != null) {
-
-			for (int i = 0; i <= this.table.length; i++) {
-				int elementHash = getHash(element, i);
-				if (this.table[elementHash] != null) {
-					if (this.table[elementHash].equals(element)) {
-						return elementHash;
-					}
-				}
-			}
-		}
-		return -1;
-
-	}
+    }
 
 }
--- roteiros/R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ roteiros/R10-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -3,37 +3,80 @@ package adt.hashtable.open;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
-public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
-		extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressQuadraticProbingImpl(int size,
-			HashFunctionClosedAddressMethod method, int c1, int c2) {
-		super(size);
-		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
-		this.initiateInternalTable(size);
-	}
+   public HashtableOpenAddressQuadraticProbingImpl(int size, HashFunctionClosedAddressMethod method, int c1, int c2) {
+      super(size);
+      hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
+      this.initiateInternalTable(size);
+   }
 
-	@Override
-	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   @Override
+   public void insert(T element) {
 
-	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+      if (isFull()) {
+         throw new HashtableOverflowException();
+      }
 
-	@Override
-	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+      if (indexOf(element) < 0) {
+         for (int i = 0; i < this.table.length; i++) {
+            int elementHash = getHash(element, i);
 
-	@Override
-	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+            if (isAvaible(elementHash)) {
+               this.table[elementHash] = element;
+               this.elements++;
+               return;
+            } else {
+               this.COLLISIONS++;
+            }
+         }
+      }
+   }
+
+   private boolean isAvaible(int elementHash) {
+      if (this.table[elementHash] instanceof HashtableElement) {
+         return false;
+      }
+      return true;
+   }
+
+   private int getHash(T element, int probe) {
+      return ((HashFunctionQuadraticProbing<T>) getHashFunction()).hash(element, probe);
+   }
+
+   @Override
+   public void remove(T element) {
+      int elementIndex = indexOf(element);
+      if (elementIndex >= 0) {
+         this.table[elementIndex] = new DELETED();
+         this.elements--;
+      }
+
+   }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public T search(T element) {
+      int elementIndex = indexOf(element);
+      if (elementIndex > 0) {
+         return (T) this.table[elementIndex];
+      }
+      return null;
+   }
+
+   @Override
+   public int indexOf(T element) {
+      if (element != null) {
+         for (int i = 0; i <= this.table.length; i++) {
+            int elementHash = getHash(element, i);
+            if (this.table[elementHash] != null) {
+               if (this.table[elementHash].equals(element)) {
+                  return elementHash;
+               }
+            }
+         }
+      }
+      return -1;
+
+   }
 }
