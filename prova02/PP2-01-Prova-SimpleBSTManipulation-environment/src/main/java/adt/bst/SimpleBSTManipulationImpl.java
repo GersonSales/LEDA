654b112c5f2dc18ddccd78ffa62dbc8c56e1b605
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2016-08-29 14:01:16 -0300

    adicao de prova02

---
+++ prova02/PP2-01-Prova-SimpleBSTManipulation-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -0,0 +1,23 @@
+package adt.bst;
+
+public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements SimpleBSTManipulation<T> {
+
+	@Override
+	public boolean equals(BST<T> tree1, BST<T> tree2) {
+		// TODO Implement this method
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
+		// TODO Implement this method
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T orderStatistic(BST<T> tree, int k) {
+		// TODO Implement this method
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+}
