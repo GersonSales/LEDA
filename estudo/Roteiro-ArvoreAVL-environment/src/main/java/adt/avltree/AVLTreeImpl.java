562d448ac47152bc17f3eadb23ab652c190cf76d
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2016-09-01 17:43:34 -0300

    adicao de roteiro AVL para estudo

--- estudo/Roteiro-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
+++ estudo/Roteiro-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -3,39 +3,39 @@ package adt.avltree;
 import adt.bst.BSTImpl;
 import adt.bst.BSTNode;
 
-public class AVLTreeImpl<T extends Comparable<T>> 
-    extends BSTImpl<T> implements AVLTree<T> {
+public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements AVLTree<T> {
 
-	//TODO Do not forget: you must override the methods insert and remove conveniently.
+	// TODO Do not forget: you must override the methods insert and remove
+	// conveniently.
 
-	//AUXILIARY
-	protected int calculateBalance(BSTNode<T> node){
+	// AUXILIARY
+	protected int calculateBalance(BSTNode<T> node) {
 		// TODO Auto-generated method stub
 		throw new RuntimeException("Not implemented yet!");
 	}
-	
-	//AUXILIARY
-	protected void rebalance(BSTNode<T> node){
+
+	// AUXILIARY
+	protected void rebalance(BSTNode<T> node) {
 		// TODO Auto-generated method stub
 		throw new RuntimeException("Not implemented yet!");
 	}
-	
-	//AUXILIARY
-	protected void rebalanceUp(BSTNode<T> node){
+
+	// AUXILIARY
+	protected void rebalanceUp(BSTNode<T> node) {
 		// TODO Auto-generated method stub
 		throw new RuntimeException("Not implemented yet!");
 	}
-	
-	//AUXILIARY
-	protected void leftRotation(BSTNode<T> node){
+
+	// AUXILIARY
+	protected void leftRotation(BSTNode<T> node) {
 		// TODO Auto-generated method stub
 		throw new RuntimeException("Not implemented yet!");
 	}
-	
-	//AUXILIARY
-	protected void rightRotation(BSTNode<T> node){
+
+	// AUXILIARY
+	protected void rightRotation(BSTNode<T> node) {
 		// TODO Auto-generated method stub
 		throw new RuntimeException("Not implemented yet!");
 	}
-	
+
 }
