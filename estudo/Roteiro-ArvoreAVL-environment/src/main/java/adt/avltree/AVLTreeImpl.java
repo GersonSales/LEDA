e93d470352dca89bda5f09ff23a06c164fb211cd
Gerson <gerson.junior@ccc.ufcg.edu.br>
2016-09-03 07:50:23 -0300

    inicio de resolução do roteiro AVL

--- estudo/Roteiro-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
+++ estudo/Roteiro-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -2,22 +2,87 @@ package adt.avltree;
 
 import adt.bst.BSTImpl;
 import adt.bst.BSTNode;
+import adt.bt.BTNode;
 
-public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements AVLTree<T> {
+public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
+		implements AVLTree<T> {
 
 	// TODO Do not forget: you must override the methods insert and remove
 	// conveniently.
 
-	// AUXILIARY
-	protected int calculateBalance(BSTNode<T> node) {
+	@Override
+	public void insert(T element) {
+		super.insert(element);
+		BTNode<T> newNode = search(element);
+		rebalance(newNode);
+	}
+
+	@Override
+	public void remove(T element) {
 		// TODO Auto-generated method stub
 		throw new RuntimeException("Not implemented yet!");
 	}
 
 	// AUXILIARY
+	protected int calculateBalance(BSTNode<T> node) {
+		return calculateBalance((BTNode<T>) node);
+	}
+
+	private int calculateBalance(BTNode<T> node) {
+		return height(node.getRight()) - height(node.getLeft());
+
+	}
+
+	// AUXILIARY
 	protected void rebalance(BSTNode<T> node) {
-		// TODO Auto-generated method stub
-		throw new RuntimeException("Not implemented yet!");
+		rebalance((BTNode<T>) node);
+	}
+
+	private void rebalance(BTNode<T> node) {
+		if (node != null && !node.isEmpty()) {
+			if (isBalanced(node)) {
+				rebalance(node.getParent());
+			} else {
+				if (isRightDisbalanced(node)) {
+					if (isLeftTilt(node.getRight())) {
+						rightRotation(node.getRight());
+						leftRotation(node);
+					} else {
+						leftRotation(node);
+					}
+
+				} else if (isLeftDisbalanced(node)) {
+					if (isRightTilt(node.getLeft())) {
+						leftRotation(node.getLeft());
+						rightRotation(node);
+					} else {
+						rightRotation(node);
+					}
+
+				}
+			}
+		}
+	}
+
+	private boolean isLeftDisbalanced(BTNode<T> node) {
+		return calculateBalance(node) < -1;
+	}
+
+	private boolean isRightDisbalanced(BTNode<T> node) {
+		return calculateBalance(node) > 1;
+	}
+
+	private boolean isBalanced(BTNode<T> node) {
+		return isLeftTilt(node) || isRightTilt(node)
+				|| calculateBalance(node) == 0;
+	}
+
+	private boolean isLeftTilt(BTNode<T> node) {
+		return calculateBalance(node) == -1;
+	}
+
+	private boolean isRightTilt(BTNode<T> node) {
+		return calculateBalance(node) == 1;
 	}
 
 	// AUXILIARY
@@ -28,14 +93,58 @@ public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements 
 
 	// AUXILIARY
 	protected void leftRotation(BSTNode<T> node) {
-		// TODO Auto-generated method stub
-		throw new RuntimeException("Not implemented yet!");
+		leftRotation((BTNode<T>) node);
+	}
+
+	private void leftRotation(BTNode<T> node) {
+		BTNode<T> nodeParent = node.getParent();
+		BTNode<T> rightNode = node.getRight();
+
+		if (isLeftChild(node)) {
+			nodeParent.setLeft(rightNode);
+		} else if (isRightChild(node)) {
+			nodeParent.setRight(rightNode);
+		}
+
+		rightNode.setParent(nodeParent);
+
+		node.setRight(rightNode.getLeft());
+		node.getRight().setParent(node);
+
+		rightNode.setLeft(node);
+		node.setParent(rightNode);
+
+		if (getRoot().equals(node)) {
+			setRoot((BSTNode<T>) rightNode);
+		}
 	}
 
 	// AUXILIARY
 	protected void rightRotation(BSTNode<T> node) {
-		// TODO Auto-generated method stub
-		throw new RuntimeException("Not implemented yet!");
+		rightRotation((BTNode<T>) node);
+	}
+
+	private void rightRotation(BTNode<T> node) {
+		BTNode<T> nodeParent = node.getParent();
+		BTNode<T> leftNode = node.getLeft();
+
+		if (isLeftChild(node)) {
+			nodeParent.setLeft(leftNode);
+		} else if (isRightChild(node)) {
+			nodeParent.setRight(leftNode);
+		}
+
+		node.setLeft(leftNode.getRight());
+		node.getLeft().setParent(node);
+
+		leftNode.setParent(nodeParent);
+
+		leftNode.setRight(node);
+		node.setParent(leftNode);
+
+		if (getRoot().equals(node)) {
+			setRoot((BSTNode<T>) leftNode);
+		}
 	}
 
 }
--- estudo/Roteiro-ArvoreAVL-environment/src/test/java/adt/avltree/StudentAVLTest.java
+++ estudo/Roteiro-ArvoreAVL-environment/src/test/java/adt/avltree/StudentAVLTest.java
@@ -1,4 +1,5 @@
 package adt.avltree;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -8,12 +9,12 @@ import adt.bst.BSTNode;
 
 public class StudentAVLTest {
 
-    private AVLTree<Integer> tree;
+	private AVLTree<Integer> tree;
 	private BSTNode<Integer> NIL = new BSTNode<Integer>();
-	
+
 	private void fillTree() {
-		
-		for(int i = 0; i < 10 ; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			tree.insert(i);
 		}
 	}
@@ -21,6 +22,127 @@ public class StudentAVLTest {
 	@Before
 	public void setUp() {
 		tree = new AVLTreeImpl<>();
+	}
+
+	@Test
+	public void sortedBalancingTest() {
+		assertTrue(tree.isEmpty());
+
+		tree.insert(1);
+		assertFalse(tree.isEmpty());
+		assertEquals(new Integer(1), tree.getRoot().getData());
+		assertEquals(null, tree.getRoot().getLeft().getData());
+		assertEquals(null, tree.getRoot().getRight().getData());
+
+		tree.insert(2);
+		assertEquals(new Integer(1), tree.getRoot().getData());
+		assertEquals(new Integer(2), tree.getRoot().getRight().getData());
+		assertEquals(null, tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(1), tree.search(2).getParent().getData());
+
+		tree.insert(3);
+		assertEquals(new Integer(2), tree.getRoot().getData());
+		assertEquals(new Integer(1), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(3), tree.getRoot().getRight().getData());
+		assertEquals(null, tree.search(2).getParent());
+		assertEquals(new Integer(2), tree.search(3).getParent().getData());
+		assertEquals(new Integer(2), tree.search(1).getParent().getData());
+
+		tree.insert(4);
+		assertEquals(new Integer(2), tree.getRoot().getData());
+		assertEquals(new Integer(1), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(3), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(3), tree.search(4).getParent().getData());
+		assertEquals(new Integer(4), tree.search(3).getRight().getData());
+
+		tree.insert(5);
+		assertEquals(new Integer(2), tree.getRoot().getData());
+		assertEquals(new Integer(1), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(4), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(4), tree.search(5).getParent().getData());
+		assertEquals(new Integer(4), tree.search(3).getParent().getData());
+		assertEquals(new Integer(3), tree.search(4).getLeft().getData());
+		assertEquals(new Integer(5), tree.search(4).getRight().getData());
+
+		tree.insert(6);
+		assertEquals(new Integer(4), tree.getRoot().getData());
+		assertEquals(new Integer(2), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(5), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(4), tree.search(2).getParent().getData());
+		assertEquals(new Integer(5), tree.search(6).getParent().getData());
+		assertEquals(new Integer(3), tree.search(2).getRight().getData());
+		assertEquals(new Integer(6), tree.search(5).getRight().getData());
+
+		tree.insert(7);
+		assertEquals(new Integer(4), tree.getRoot().getData());
+		assertEquals(new Integer(2), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(6), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(4), tree.search(6).getParent().getData());
+		assertEquals(new Integer(6), tree.search(7).getParent().getData());
+		assertEquals(new Integer(5), tree.search(6).getLeft().getData());
+		assertEquals(new Integer(7), tree.search(6).getRight().getData());
+
+	}
+
+	@Test
+	public void unsortedBalancingTes() {
+
+		tree.insert(36);
+		assertEquals(new Integer(36), tree.getRoot().getData());
+		assertEquals(null, tree.getRoot().getLeft().getData());
+		assertEquals(null, tree.getRoot().getRight().getData());
+		assertEquals(null, tree.search(36).getParent());
+
+		tree.insert(3);
+		assertEquals(new Integer(36), tree.getRoot().getData());
+		assertEquals(new Integer(3), tree.getRoot().getLeft().getData());
+		assertEquals(null, tree.getRoot().getRight().getData());
+		assertEquals(new Integer(36), tree.search(3).getParent().getData());
+		assertEquals(new Integer(3), tree.search(36).getLeft().getData());
+
+		tree.insert(72);
+		assertEquals(new Integer(36), tree.getRoot().getData());
+		assertEquals(new Integer(3), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(72), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(36), tree.search(72).getParent().getData());
+		assertEquals(null, tree.search(3).getLeft().getData());
+		assertEquals(null, tree.search(3).getRight().getData());
+
+		tree.insert(31);
+		assertEquals(new Integer(36), tree.getRoot().getData());
+		assertEquals(new Integer(3), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(72), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(3), tree.search(31).getParent().getData());
+		assertEquals(new Integer(31), tree.search(3).getRight().getData());
+
+		tree.insert(74);
+		tree.insert(78);
+		tree.insert(76);
+		tree.insert(68);
+
+		tree.insert(65);
+		assertEquals(new Integer(36), tree.getRoot().getData());
+		assertEquals(new Integer(3), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(74), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(68),
+				tree.getRoot().getRight().getLeft().getData());
+		assertEquals(new Integer(68), tree.search(65).getParent().getData());
+		assertEquals(new Integer(74), tree.search(68).getParent().getData());
+		assertEquals(new Integer(68), tree.search(65).getParent().getData());
+		assertEquals(new Integer(65), tree.search(68).getLeft().getData());
+		assertEquals(new Integer(72), tree.search(68).getRight().getData());
+
+		tree.insert(63);
+		assertEquals(new Integer(68), tree.getRoot().getData());
+		assertEquals(new Integer(36), tree.getRoot().getLeft().getData());
+		assertEquals(new Integer(74), tree.getRoot().getRight().getData());
+		assertEquals(new Integer(72),
+				tree.getRoot().getRight().getLeft().getData());
+		assertEquals(new Integer(68), tree.search(36).getParent().getData());
+		assertEquals(new Integer(65), tree.search(63).getParent().getData());
+		assertEquals(new Integer(68), tree.search(74).getParent().getData());
+		assertEquals(new Integer(72), tree.search(74).getLeft().getData());
+		assertEquals(new Integer(65), tree.search(36	).getRight().getData());
 	}
 
 	@Test
@@ -77,7 +199,7 @@ public class StudentAVLTest {
 	@Test
 	public void testSucessorPredecessor() {
 
-		fillTree(); 
+		fillTree();
 
 		assertEquals(null, tree.predecessor(15));
 		assertEquals(new Integer(3), tree.sucessor(2).getData());
@@ -88,12 +210,12 @@ public class StudentAVLTest {
 
 	@Test
 	public void testSize() {
-		fillTree(); 
+		fillTree();
 
 		int size = 10;
 		assertEquals(size, tree.size());
 
-		while(!tree.isEmpty()) {
+		while (!tree.isEmpty()) {
 			tree.remove(tree.getRoot().getData());
 			assertEquals(--size, tree.size());
 		}
@@ -101,9 +223,9 @@ public class StudentAVLTest {
 
 	@Test
 	public void testHeight() {
-		fillTree(); 
+		fillTree();
 
-		Integer[] preOrder = new Integer[] {3,1,0,2,7,5,4,6,8,9};
+		Integer[] preOrder = new Integer[] { 3, 1, 0, 2, 7, 5, 4, 6, 8, 9 };
 		assertArrayEquals(preOrder, tree.preOrder());
 		assertEquals(3, tree.height());
 
@@ -112,23 +234,24 @@ public class StudentAVLTest {
 
 		tree.remove(2);
 		assertEquals(3, tree.height());
-		
+
 		tree.remove(1);
 		assertEquals(3, tree.height());
-		Integer[] preOrder2 = new Integer[] {7,5,3,4,6,8,9};
+		Integer[] preOrder2 = new Integer[] { 7, 5, 3, 4, 6, 8, 9 };
 		assertArrayEquals(preOrder2, tree.preOrder());
 	}
 
 	@Test
 	public void testRemove() {
-		fillTree(); 
-		//tente remover elementos e verificar se as rotacoes produzem uma AVL correta
+		fillTree();
+		// tente remover elementos e verificar se as rotacoes produzem uma AVL
+		// correta
 	}
 
 	@Test
 	public void testSearch() {
 
-		fillTree(); 
+		fillTree();
 
 		assertEquals(NIL, tree.search(-40));
 		assertEquals(new Integer(4), tree.search(4).getData());
